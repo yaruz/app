c12a0424b41c076a241176e4d86505af307357f9
Andrey Kalinin <kalinin.andrey1@mail.ru>
2021-04-04 22:43:40 +0300

    entity_type

---
+++ pkg/yaruz_data/domain/entity_type/entity.go
@@ -0,0 +1,29 @@
+package entity_type
+
+import "time"
+
+const (
+	EntityName = "entityType"
+	TableName  = "entity_type"
+)
+
+// EntityType ...
+type EntityType struct {
+	ID          uint       `gorm:"primaryKey" json:"id"`
+	Sysname     string     `gorm:"type:varchar(100) not null;unique;index" json:"sysname"`
+	TSouirceID  uint       `gorm:"index" json:"tSourceId"`
+	Name        string     `gorm:"-" json:"name"`
+	Description string     `gorm:"-" json:"description"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
+	DeletedAt   *time.Time `gorm:"index" json:"deletedAt,omitempty"`
+}
+
+func (e EntityType) TableName() string {
+	return TableName
+}
+
+// New func is a constructor for the EntityType
+func New() *EntityType {
+	return &EntityType{}
+}
